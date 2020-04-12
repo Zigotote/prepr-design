fa466d1f7c44f9bcc29e92554e96017d780d8174
Zigotote <oce88b@gmail.com>
2020-04-12 19:08:30 +0200

    Add profile menu

--- src/App.vue
+++ src/App.vue
@@ -8,35 +8,27 @@
         <b-row>
           <b-col>
             <b-button size="lg">
-              <router-link to="/" class="font-black"> Explore </router-link>
+              <router-link to="/" class="font-black">Explore</router-link>
             </b-button>
           </b-col>
           <b-col>
             <b-button size="lg">
-              <router-link to="/labs" class="font-black">
-                Labs
-              </router-link>
+              <router-link to="/labs" class="font-black">Labs</router-link>
             </b-button>
           </b-col>
           <b-col>
             <b-button size="lg">
-              <router-link to="/organisations" class="font-black">
-                Organisations
-              </router-link>
+              <router-link to="/organisations" class="font-black">Organisations</router-link>
             </b-button>
           </b-col>
           <b-col>
             <b-button size="lg">
-              <router-link to="/challenges" class="font-black">
-                Challenges
-              </router-link>
+              <router-link to="/challenges" class="font-black">Challenges</router-link>
             </b-button>
           </b-col>
           <b-col>
             <b-button size="lg">
-              <router-link to="resources" class="font-black">
-                Resources
-              </router-link>
+              <router-link to="resources" class="font-black">Resources</router-link>
             </b-button>
           </b-col>
         </b-row>
@@ -49,13 +41,47 @@
             </p>
           </b-col>
           <b-col>
-            <a href="/">
-              <img
-                src="./assets/default-user.png"
-                alt="User profile"
-                id="user-img"
-              />
-            </a>
+            <b-button class="bg-white" variant="light" v-b-toggle.profile-menu>
+              <img src="./assets/default-user.png" alt="User profile" id="user-img" />
+            </b-button>
+            <b-sidebar id="profile-menu" right shadow>
+              <div class="px-3 py-2">
+                <b-list-group>
+                  <b-list-group-item class="d-flex align-items-center">
+                    <b-icon icon="person-fill"></b-icon>
+                    <div class="ml-2">My profile</div>
+                  </b-list-group-item>
+                  <b-list-group-item class="d-flex align-items-center">
+                    <img src="./assets/menu-icon/projects.png" class="icon" />
+                    <div class="ml-2">My projects</div>
+                  </b-list-group-item>
+                  <b-list-group-item class="d-flex align-items-center">
+                    <img src="./assets/menu-icon/labs.png" class="icon" />
+                    <div class="ml-2">My labs</div>
+                  </b-list-group-item>
+                  <b-list-group-item class="d-flex align-items-center">
+                    <b-icon icon="newspaper"></b-icon>
+                    <div class="ml-2">News feed</div>
+                  </b-list-group-item>
+                  <b-list-group-item class="d-flex align-items-center">
+                    <b-icon icon="inbox"></b-icon>
+                    <div class="ml-2">Inbox</div>
+                  </b-list-group-item>
+                  <b-list-group-item class="d-flex align-items-center">
+                    <b-icon icon="gear"></b-icon>
+                    <div class="ml-2">Settings</div>
+                  </b-list-group-item>
+                  <b-list-group-item class="d-flex align-items-center">
+                    <img src="./assets/menu-icon/help.png" class="icon" />
+                    <div class="ml-2">Help</div>
+                  </b-list-group-item>
+                  <b-list-group-item class="d-flex align-items-center">
+                    <img src="./assets/menu-icon/log_out.png" class="icon" />
+                    <div class="ml-2">Log out</div>
+                  </b-list-group-item>
+                </b-list-group>
+              </div>
+            </b-sidebar>
           </b-col>
         </b-row>
       </b-container>
@@ -64,12 +90,8 @@
     <router-view />
     <b-container id="footer" fluid>
       <b-row class="justify-content-md-center">
-        <b-col md="auto">
-          Privacy policy
-        </b-col>
-        <b-col md="auto">
-          Term of use
-        </b-col>
+        <b-col md="auto">Privacy policy</b-col>
+        <b-col md="auto">Term of use</b-col>
       </b-row>
     </b-container>
   </b-container>
@@ -94,6 +116,10 @@ export default {};
 
 #user-img {
   height: 65px;
+}
+
+.icon {
+  width: 5%;
 }
 
 #footer {
